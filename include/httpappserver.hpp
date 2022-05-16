6059f4634cbafe4f0640fa78bb99ce44e0011c6f
Luke Tokheim <luketokheim@gmail.com>
2022-05-16 16:10:54 -0700

    Move functions into source file. Allow for clean exit on SIGTERM and SIGINT
    - Refactor into public API later when adding handler functions
    - Test coverage will also need access
    - Added signal "watchdog" to the socket listener

--- include/httpappserver.hpp
+++ include/httpappserver.hpp
@@ -1,25 +1,8 @@
 #pragma once
 
-/// Set _WIN32_WINNT to the default for current Windows SDK
-#if defined(_WIN32) && !defined(_WIN32_WINNT)
-#include <SDKDDKVer.h>
-#endif
-
-#include <boost/asio/awaitable.hpp>
-#include <boost/asio/ip/tcp.hpp>
-
 #include <string_view>
 
 namespace httpappserver {
-
-namespace asio = boost::asio;
-using tcp = asio::ip::tcp;
-
-asio::awaitable<void> session(tcp::socket socket);
-
-asio::awaitable<void> session_ec(tcp::socket socket);
-
-asio::awaitable<void> listen(tcp::endpoint endpoint);
 
 int run(std::string_view host, std::string_view service);
 
