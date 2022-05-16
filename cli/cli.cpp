765355a134d945e6bdb6898ed2e356e9aad5cde1
Luke Tokheim <luketokheim@gmail.com>
2022-05-16 14:44:58 -0700

    Rename "service" to "port" since we will not be using well known service names like "http".
    - Load the PORT environment variable when parsing command line options
    - PORT=NUM in env is lower priority than command line --port NUM

--- cli/cli.cpp
+++ cli/cli.cpp
@@ -1,22 +1,27 @@
 #include <httpappserver.hpp>
 
+#include <cstdlib>
 #include <iostream>
 #include <optional>
 
-struct command_line_options {
+struct cli_options {
     std::string_view host = "0.0.0.0";
-    std::string_view service = "8080";
+    std::string_view port = "8080";
 };
 
-std::optional<command_line_options> make_options(int argc, char **argv)
+std::optional<cli_options> make_options(int argc, char **argv)
 {
-    command_line_options options;
+    cli_options options;
+    
+    if (const char* port = std::getenv("PORT")) {
+        options.port = port;
+    }
 
     for (int i = 1; i < argc; ++i) {
         std::string_view name(argv[i]);
         if (name == "--port") {
             if (++i < argc) {
-                options.service = argv[i];
+                options.port = argv[i];
                 continue;
             }
         } else if (name == "--host") {
@@ -38,14 +43,14 @@ int main(int argc, char **argv)
     const auto options = make_options(argc, argv);
     if (!options) {
         std::cout << "Usage: cli [OPTION]...\n\n"
-                     "Available options:\n"
-                     "  --host HOST[=0.0.0.0]\n"
-                     "  --port NUMBER[=8080]\n";
+                     "Options:\n"
+                     "  --host arg (=0.0.0.0)\n"
+                     "  --port arg (=8080)\n";
         return -1;
     }
 
     try {
-        return httpappserver::run(options->host, options->service);
+        return httpappserver::run(options->host, options->port);
     } catch (std::exception &e) {
         std::cerr << e.what() << "\n";
     } catch (...) {
