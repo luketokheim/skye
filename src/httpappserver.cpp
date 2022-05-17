220b644f18942ee721f4ff902674b9baaea6f165
Luke Tokheim <luketokheim@gmail.com>
2022-05-16 21:03:12 -0700

    Using asio sync calls with one thread per http client to compare

--- src/httpappserver.cpp
+++ src/httpappserver.cpp
@@ -15,8 +15,15 @@
 #include <boost/asio/signal_set.hpp>
 #include <boost/beast/core.hpp>
 #include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
+#include <boost/lexical_cast.hpp>
 
+#include <array>
+#include <chrono>
 #include <exception>
+#include <future>
+#include <thread>
+#include <vector>
 
 namespace httpappserver {
 
@@ -58,6 +65,46 @@ asio::awaitable<void> session(tcp::socket socket)
     }
 
     socket.shutdown(tcp::socket::shutdown_send);
+}
+
+void sync_session(tcp::socket &&socket)
+{
+    beast::error_code ec;
+
+    for (;;) {
+        http::request<http::string_body> req;
+        {
+            beast::flat_buffer buffer;
+            http::read(socket, buffer, req, ec);
+
+            if (ec == http::error::end_of_stream) {
+                break;
+            }
+
+            if (ec) {
+                return;
+            }
+        }
+
+        const bool keep_alive = req.keep_alive();
+
+        auto res = make_response(std::move(req));
+        res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+        res.prepare_payload();
+        res.keep_alive(keep_alive);
+
+        http::write(socket, res, ec);
+
+        if (ec) {
+            return;
+        }
+
+        if (res.need_eof()) {
+            break;
+        }
+    }
+
+    socket.shutdown(tcp::socket::shutdown_send, ec);
 }
 
 asio::awaitable<void> session_ec(tcp::socket socket)
@@ -106,10 +153,15 @@ asio::awaitable<void> listen(tcp::endpoint endpoint)
 {
     tcp::acceptor acceptor{co_await asio::this_coro::executor, endpoint};
     for (;;) {
-        auto socket = co_await acceptor.async_accept(asio::use_awaitable);
+        beast::error_code ec;
+        auto socket = co_await acceptor.async_accept(
+            asio::redirect_error(asio::use_awaitable, ec));
+        if (ec) {
+            break;
+        }
 
         co_spawn(
-            acceptor.get_executor(), session(std::move(socket)),
+            acceptor.get_executor(), session_ec(std::move(socket)),
             asio::detached);
     }
 }
@@ -132,17 +184,112 @@ int run(std::string_view host, std::string_view port)
 {
     asio::io_context ctx;
 
+#if 0
+
+    {
+        auto endpoint =
+            *tcp::resolver(ctx).resolve(host, port, tcp::resolver::passive);
+
+        co_spawn(ctx, run_server(std::move(endpoint)), [](auto ptr) {
+            // Propagate exception from the coroutine
+            if (ptr) {
+                std::rethrow_exception(ptr);
+            }
+        });
+    }
+
+    std::vector<std::jthread> thread_list;
+    {
+        const auto num_thread = std::thread::hardware_concurrency();
+        if (num_thread > 1) {
+            thread_list.resize(num_thread - 1);
+
+            std::generate(thread_list.begin(), thread_list.end(), [&ctx]() {
+                return std::jthread([&ctx]() { ctx.run(); });
+            });
+        }
+    }
+
+#else
+    using namespace std::chrono_literals;
+
+    auto cleanup = [](auto pool, auto duration) {
+        std::vector<std::future<void>> busy;
+        for (auto &future : pool) {
+            if (future.wait_for(duration) == std::future_status::ready) {
+                future.get();
+            } else {
+                busy.push_back(std::move(future));
+            }
+        }
+
+        return busy;
+    };
+
     auto endpoint =
         *tcp::resolver(ctx).resolve(host, port, tcp::resolver::passive);
 
-    co_spawn(ctx, run_server(std::move(endpoint)), [](auto ptr) {
-        // Propagate exception from the coroutine
-        if (ptr) {
-            std::rethrow_exception(ptr);
-        }
-    });
+    tcp::acceptor acceptor{ctx, endpoint};
 
-    ctx.run();
+    std::vector<std::future<void>> pool;
+    //std::array<std::future<void>, 2> pool;
+   //auto last = pool.begin();
+
+    for (;;) {
+        auto socket = acceptor.accept();
+
+        auto future =
+            std::async(std::launch::async, sync_session, std::move(socket));
+
+        if (pool.size() >= 80) {
+            pool = cleanup(std::move(pool), 0ms);
+
+            while (pool.size() >= 160) {
+                pool = cleanup(std::move(pool), 1s);
+            }
+        }
+
+        pool.push_back(std::move(future));
+
+        /*
+        if (last == pool.end()) {
+            for (auto &item : pool) {
+                if (item.wait_for(1s) != std::future_status::ready) {
+                    continue;
+                } 
+
+                item.get();
+
+                std::swap(item, future);
+                break;
+            }
+        } else {
+            *last++ = std::move(future);
+        }
+        */
+        /*
+        if (last - pool.begin() >= 4) {
+            for (auto itr = pool.begin(); itr != last; ++itr) {
+                if (itr->wait_for(0ms) != std::future_status::ready) {
+                    continue;
+                }
+
+                itr->get();
+                std::iter_swap(itr, last);
+                
+                if (itr == --last) {
+                    break;
+                }
+            }
+        }
+        */
+
+        //if (next == future_list.size()) {
+        
+        //}
+    }
+
+#endif
 
     return 0;
 }
