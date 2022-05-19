5e7fc9de3363cb936782e84df57018b96a57b3bd
Luke Tokheim <luketokheim@gmail.com>
2022-05-19 10:32:47 -0700

    Create session_stats class to store info about a http stream, for testing keep-alive and duration

--- src/httpmicroservice.cpp
+++ src/httpmicroservice.cpp
@@ -9,7 +9,11 @@
 #include <boost/beast/core.hpp>
 #include <boost/beast/http.hpp>
 
+#include <chrono>
 #include <exception>
+#include <iostream>
+#include <optional>
+#include <thread>
 
 namespace httpmicroservice {
 
@@ -23,23 +27,58 @@ response_type make_response(request_type req)
     return response_type(http::status::ok, req.version());
 }
 
-asio::awaitable<void> session(tcp::socket socket, handler_type handler)
+struct session_stats {
+    std::thread::id tid;
+    int fd = 0;
+    int num_request = 0;
+    int bytes_transferred = 0;
+    std::chrono::steady_clock::duration duration;
+};
+
+std::ostream &operator<<(std::ostream &os, const session_stats &stats)
 {
+    os << "{\"tid\": " << stats.tid << ", \"fd\": " << stats.fd
+       << ", \"req\": " << stats.num_request
+       << ", \"bytes\": " << stats.bytes_transferred
+       << ", \"duration\": " << stats.duration << "}";
+    return os;
+}
+
+asio::awaitable<std::optional<session_stats>> session(
+    tcp::socket socket, handler_type handler,
+    std::optional<session_stats> stats)
+{
+    std::chrono::steady_clock::time_point start;
+    if (stats) {
+        stats->tid = std::this_thread::get_id();
+        stats->fd = socket.native_handle();
+        start = std::chrono::steady_clock::now();
+    }
+
     boost::beast::flat_buffer buffer(kRequestSizeLimit);
     boost::system::error_code ec;
 
     for (;;) {
         // req = read(...)
         request_type req;
-        co_await http::async_read(
-            socket, buffer, req, asio::redirect_error(asio::use_awaitable, ec));
+        {
+            auto bytes_read = co_await http::async_read(
+                socket, buffer, req,
+                asio::redirect_error(asio::use_awaitable, ec));
 
-        if (ec == http::error::end_of_stream) {
-            break;
-        }
+            // if (ec == http::error::end_of_stream) {
+            //     socket.shutdown(tcp::socket::shutdown_send, ec);
+            //     break;
+            // }
 
-        if (ec) {
-            co_return;
+            if (ec) {
+                break;
+            }
+
+            if (stats) {
+                ++stats->num_request;
+                stats->bytes_transferred += bytes_read;
+            }
         }
 
         auto keep_alive = req.keep_alive();
@@ -50,27 +89,28 @@ asio::awaitable<void> session(tcp::socket socket, handler_type handler)
         res.keep_alive(keep_alive);
 
         // write(res)
-        co_await http::async_write(
+        auto bytes_write = co_await http::async_write(
             socket, res, asio::redirect_error(asio::use_awaitable, ec));
 
         if (ec) {
-            co_return;
+            co_return stats;
+        }
+
+        if (stats) {
+            stats->bytes_transferred += bytes_write;
         }
 
         if (res.need_eof()) {
+            socket.shutdown(tcp::socket::shutdown_send, ec);
             break;
         }
     }
 
-    socket.shutdown(tcp::socket::shutdown_send, ec);
-}
-
-void rethrow_exception_ptr(std::exception_ptr ptr)
-{
-    // Propagate exception from a coroutine
-    if (ptr) {
-        std::rethrow_exception(ptr);
+    if (stats) {
+        stats->duration = std::chrono::steady_clock::now() - start;
     }
+
+    co_return stats;
 }
 
 asio::awaitable<void> listen(int port, handler_type handler)
@@ -80,16 +120,27 @@ asio::awaitable<void> listen(int port, handler_type handler)
     tcp::acceptor acceptor(co_await asio::this_coro::executor, endpoint);
     for (;;) {
         boost::system::error_code ec;
+
         auto socket = co_await acceptor.async_accept(
             asio::redirect_error(asio::use_awaitable, ec));
+
         if (ec) {
             break;
         }
 
         // Run coroutine to handle one http connection
         co_spawn(
-            acceptor.get_executor(), session(std::move(socket), handler),
-            rethrow_exception_ptr);
+            acceptor.get_executor(), session(std::move(socket), handler, {}),
+            [](auto ptr, auto stats) {
+                // Propagate exception from the coroutine
+                if (ptr) {
+                    std::rethrow_exception(ptr);
+                }
+
+                if (stats) {
+                    std::cout << *stats << "\n";
+                }
+            });
     }
 }
 
@@ -98,7 +149,12 @@ int run(int port, handler_type handler)
     asio::io_context ctx;
 
     // Run coroutine to listen on our port
-    co_spawn(ctx, listen(port, std::move(handler)), rethrow_exception_ptr);
+    co_spawn(ctx, listen(port, std::move(handler)), [](auto ptr) {
+        // Propagate exception from the coroutine
+        if (ptr) {
+            std::rethrow_exception(ptr);
+        }
+    });
 
     ctx.run();
 
