71b98803aaa9e4093306d53cd3aab53e5c7e836e
Luke Tokheim <luketokheim@gmail.com>
2022-05-20 19:14:15 -0700

    Working on sanitizer build

--- ci/build_sanitizer.sh
+++ ci/build_sanitizer.sh
@@ -1,12 +1,18 @@
-conan profile update settings.build_type=Debug default
-conan profile update settings.compiler=clang default
-conan profile update settings.compiler.version=14 default
-conan profile update settings.compiler.libcxx=libc++ default
-conan install .. --build=missing
+#!/bin/sh
 
-cmake -B . -S .. -GNinja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_CXX_EXTENSIONS=OFF -DENABLE_FUZZER=OFF
+conan profile new sanitizer --detect
+conan profile update settings.build_type=Debug sanitizer
+conan profile update settings.compiler=clang sanitizer
+conan profile update settings.compiler.version=14 sanitizer
+conan profile update settings.compiler.libcxx=libc++ sanitizer
+conan install .. --build=missing --profile=sanitizer
 
- -DCMAKE_CXX_FLAGS="-fsanitize=fuzzer-no-link,address,signed-integer-overflow,null" 
+cmake -B . -S .. -GNinja \
+  -DCMAKE_BUILD_TYPE=Debug \
+  -DCMAKE_CXX_COMPILER=clang++-14 \
+  -DCMAKE_CXX_FLAGS="-fsanitize=address,thread,signed-integer-overflow,null" \
+  -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+  -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake
 
 cmake --build . --config Debug
 
