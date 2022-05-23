6435a41d7d902380f987973352679f1fc3d20902
Luke Tokheim <luketokheim@gmail.com>
2022-05-23 14:26:08 -0700

    Add clang-tidy ci script,

---
+++ ci/build_tidy.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+conan profile new tidy --detect
+conan profile update settings.build_type=Debug tidy
+conan profile update settings.compiler=clang tidy
+conan profile update settings.compiler.version=14 tidy
+conan profile update settings.compiler.libcxx=libc++ tidy
+conan install .. --build=missing --profile=tidy
+
+cmake -B . -S .. -GNinja \
+  -DCMAKE_BUILD_TYPE=Debug \
+  -DCMAKE_CXX_COMPILER=clang++ \
+  -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+  -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake \
+  -DENABLE_TESTING=OFF
+
+cmake --build . --config Debug
+
+ctest -C Debug
