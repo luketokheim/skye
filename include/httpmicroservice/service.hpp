c30a3b81cc163e53aaa3239a106947032d4b629f
Luke Tokheim <luketokheim@gmail.com>
2022-05-26 17:14:55 -0700

    Use co_await on request handlers
    - Allow for async I/O inside a handler
    - Allow for sync API calls or other blocking tasks inside a handler that are executed in a thread pool or other context

--- include/httpmicroservice/service.hpp
+++ include/httpmicroservice/service.hpp
@@ -4,6 +4,7 @@
 
 #include <boost/asio/awaitable.hpp>
 #include <boost/asio/co_spawn.hpp>
+#include <boost/asio/detached.hpp>
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/ip/tcp.hpp>
 #include <boost/asio/use_awaitable.hpp>
@@ -16,7 +17,7 @@ namespace httpmicroservice {
 namespace asio = boost::asio;
 
 template <typename Acceptor, typename Handler>
-asio::awaitable<void> accept(Acceptor acceptor, Handler handler)
+asio::awaitable<void> accept(Acceptor acceptor, Handler &&handler)
 {
     for (;;) {
         boost::system::error_code ec;
@@ -28,27 +29,30 @@ asio::awaitable<void> accept(Acceptor acceptor, Handler handler)
             break;
         }
 
+        // Bind the handler by value into the session coroutine
+        Handler session_handler(handler);
+
         // Run coroutine to handle one http connection
         co_spawn(
             acceptor.get_executor(),
             session(
-                std::move(stream), handler,
+                std::move(stream), std::move(session_handler),
                 std::make_optional<session_stats>()),
             [](auto ptr, auto stats) {
-                // Propagate exception from the coroutine
                 if (ptr) {
                     std::rethrow_exception(ptr);
                 }
 
                 if (stats) {
-                    std::cout << to_string(*stats) << "\n";
+                    // Use std::endl to flush the stream
+                    std::cout << *stats << std::endl;
                 }
             });
     }
 }
 
 template <typename Handler>
-asio::awaitable<void> listen(int port, Handler handler)
+asio::awaitable<void> listen(int port, Handler &&handler)
 {
     using tcp = asio::ip::tcp;
 
@@ -56,19 +60,29 @@ asio::awaitable<void> listen(int port, Handler handler)
 
     tcp::acceptor acceptor(co_await asio::this_coro::executor, endpoint);
 
-    co_await accept(std::move(acceptor), std::move(handler));
+    co_await accept(std::move(acceptor), std::forward<Handler>(handler));
 }
 
 template <typename Executor, typename Handler>
-void async_run(Executor ex, int port, Handler handler)
+void async_run(Executor ex, int port, Handler &&handler)
 {
     // Run coroutine to listen on our port
-    co_spawn(ex, listen(port, std::move(handler)), [](auto ptr) {
+    co_spawn(ex, listen(port, std::forward<Handler>(handler)), [](auto ptr) {
         // Propagate exception from the coroutine
         if (ptr) {
             std::rethrow_exception(ptr);
         }
     });
+}
+
+template <typename Executor, typename Handler>
+auto make_handler(Executor ex, Handler handler)
+{
+    return [ex, handler](request req) -> asio::awaitable<response> {
+        auto res =
+            co_await co_spawn(ex, handler(std::move(req)), asio::use_awaitable);
+        co_return res;
+    };
 }
 
 } // namespace httpmicroservice
