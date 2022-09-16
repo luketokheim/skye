407f964ee1e72e7f213d0d710889b231c89b506a
Luke Tokheim <luketokheim@gmail.com>
2022-09-16 10:14:58 -0700

    Bump Boost to 1.80

--- tools/build_test.sh
+++ tools/build_test.sh
@@ -3,10 +3,11 @@
 conan profile new default --detect
 conan install .. --build=missing
 
-cmake -B . -S .. \
+cmake .. \
+  -GNinja \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake
 
-cmake --build . --config Release
+cmake --build . --config Release -j 8
 
 ctest -C Release
