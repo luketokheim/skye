3f6feefad86c73032c1420483dd27a5a00d7b397
Luke Tokheim <luketokheim@gmail.com>
2022-10-24 15:25:38 -0700

    Linux port. Added get_current_cpu helper function

--- examples/threaded.cpp
+++ examples/threaded.cpp
@@ -13,12 +13,17 @@ namespace asio = boost::asio;
 namespace usrv = httpmicroservice;
 namespace http = boost::beast::http;
 
+int get_current_cpu();
+
+bool set_thread_affinity(int i);
+
 asio::awaitable<usrv::response> threaded(usrv::request req)
 {
     usrv::response res(http::status::ok, req.version());
     res.set(http::field::content_type, "application/json");
-    res.body() =
-        fmt::format("{{\"threaded\": \"{}\"}}", std::this_thread::get_id());
+    res.body() = fmt::format(
+        "{{\"thread\": \"{}\", \"cpu\": {}}}", std::this_thread::get_id(),
+        get_current_cpu());
 
     co_return res;
 }
@@ -28,18 +33,6 @@ asio::awaitable<usrv::response> threaded(usrv::request req)
 void reporter(const usrv::session_stats& stats)
 {
     fmt::print("{}\n", stats);
-}
-
-bool set_thread_affinity(int i)
-{
-    if (i < 0 || i > sizeof(DWORD_PTR)) {
-        return false;
-    }
-
-    auto x = sizeof(DWORD_PTR);
-
-    DWORD_PTR mask = static_cast<DWORD_PTR>(1) << i;
-    return SetThreadAffinityMask(GetCurrentThread(), mask) != 0;
 }
 
 int main()
@@ -62,12 +55,15 @@ int main()
         std::vector<std::jthread> thread_list;
         if (num_thread > 1) {
             if (!set_thread_affinity(0)) {
+                fmt::print(stderr, "failed to set thread {} affinity", 0);
                 return -1;
             }
 
             for (int i = 1; i < num_thread; ++i) {
                 thread_list.push_back(std::jthread{[&ioc, i]() {
                     if (!set_thread_affinity(i)) {
+                        fmt::print(
+                            stderr, "failed to set thread {} affinity", i);
                         return;
                     }
 
@@ -86,4 +82,36 @@ int main()
     }
 
     return -1;
+}
+
+int get_current_cpu()
+{
+#if defined(_WIN32)
+    return GetCurrentProcessorNumber();
+#else
+    return sched_getcpu();
+#endif
+}
+
+bool set_thread_affinity(int i)
+{
+#if defined(_WIN32)
+    if ((i < 0) || (i >= sizeof(DWORD_PTR) * 8)) {
+        return false;
+    }
+
+    DWORD_PTR mask = static_cast<DWORD_PTR>(1) << i;
+
+    return SetThreadAffinityMask(GetCurrentThread(), mask) != 0;
+#else
+    if ((i < 0) || (i >= CPU_SETSIZE)) {
+        return false;
+    }
+
+    cpu_set_t cpuset;
+    CPU_ZERO(&cpuset);
+    CPU_SET(i, &cpuset);
+
+    return pthread_setaffinity_np(pthread_self(), sizeof(cpuset), &cpuset) == 0;
+#endif
 }
