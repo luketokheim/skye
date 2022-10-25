79923d6d68fe4010060015118eff6522674d152f
Luke Tokheim <luketokheim@gmail.com>
2022-10-25 13:18:57 -0700

    Do not throw invalid_argument from getenv_port.
    Instead, return the default

--- include/httpmicroservice/service.hpp
+++ include/httpmicroservice/service.hpp
@@ -135,8 +135,8 @@ auto make_co_handler(Executor ex, Handler handler)
 }
 
 /**
-  Read the PORT environment variable. Returns 8080 if it is not set. Throws
-  exception if the PORT variable is not an integer between 1024 and 65535.
+  Read the PORT environment variable. Returns 8080 if the PORT variable is not
+  set or is not an integer between 1024 and 65535.
 
   Cloud Run sets the PORT environment variable.
   https://cloud.google.com/run/docs/container-contract#port
