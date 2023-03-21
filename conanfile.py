39db298fd03cbcdd802d1086422beb3cda04f7ef
Luke Tokheim <luketokheim@gmail.com>
2023-03-21 14:01:59 -0700

    Feature: Conan 2 support
    - Improve conanfile and cmake interop
    - Fixed tidy and sanitizer actions

--- conanfile.py
+++ conanfile.py
@@ -5,26 +5,30 @@ from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
 class SkyeRecipe(ConanFile):
     name = "skye"
     version = "0.9.0-alpha"
-    description = "Skye is an HTTP microservice framework for C++20"
+    description = "Skye is an HTTP server framework for C++20."
     homepage = "https://github.com/luketokheim/skye"
     license = "BSL"
 
     # Binary configuration
     settings = "os", "arch", "compiler", "build_type"
     options = {
+        "enable_standalone": [True, False],
+        "enable_io_uring": [True, False],
         "with_boost": [True, False],
         "with_fmt": [True, False],
         "with_sqlite3": [True, False]
     }
     default_options = {
+        "enable_standalone": False,
+        "enable_io_uring": False,
         "with_boost": True,
         "with_fmt": True,
         "with_sqlite3": False,
         "boost*:header_only": True
     }
 
-    # Sources are located in the same place as this recipe, copy them to the recipe
-    exports_sources = "CMakeLists.txt", "examples/*", "include/*", "src/*", "tests/*"
+    # Copy sources to when building this recipe for the local cache
+    exports_sources = "CMakeLists.txt", "include/*", "src/*", "examples/*", "tests/*"
 
     def layout(self):
         cmake_layout(self)
@@ -44,18 +48,26 @@ class SkyeRecipe(ConanFile):
 
     def generate(self):
         tc = CMakeToolchain(self)
+        # tc.variables["ENABLE_STANDALONE"] = self.options.enable_standalone
+        # tc.variables["ENABLE_IO_URING"] = self.options.enable_io_uring
         tc.generate()
+
         deps = CMakeDeps(self)
         deps.generate()
 
     def build(self):
+        variables = {
+            "ENABLE_STANDALONE": self.options.enable_standalone,
+            "ENABLE_IO_URING": self.options.enable_io_uring
+        }
+
         cmake = CMake(self)
-        cmake.configure()
+        cmake.configure(variables=variables)
         cmake.build()
 
     def package(self):
         cmake = CMake(self)
         cmake.install()
 
-    #def package_info(self):
-    #    self.cpp_info.libs = ["skye"]
+    def package_info(self):
+        self.cpp_info.libs = ["skye"]
