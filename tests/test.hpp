3120c57dcf5b48cce53c6164eb5ff2651f424336
Luke Tokheim <luketokheim@gmail.com>
2023-04-14 07:44:48 -0700

    Require char buffer types in test::make_random_string

--- tests/test.hpp
+++ tests/test.hpp
@@ -2,19 +2,22 @@
 
 #include <algorithm>
 #include <random>
+#include <type_traits>
 
 namespace test {
 
 template <typename Buffer>
-Buffer make_random_string(std::size_t n)
+Buffer make_random_string(std::size_t count)
 {
-    std::random_device rd;
-    std::mt19937 gen(rd());
+    static_assert(std::is_same_v<char, typename Buffer::value_type>);
 
-    std::uniform_int_distribution<int> dis(32, 127);
+    std::mt19937 gen{std::random_device{}()};
 
-    Buffer buf(n, 0);
-    std::generate(std::begin(buf), std::end(buf), [&]() { return dis(gen); });
+    std::uniform_int_distribution<int> dis{32, 127};
+
+    Buffer buf(count, 0);
+    std::generate(
+        buf.begin(), buf.end(), [&]() { return static_cast<char>(dis(gen)); });
 
     return buf;
 }
